59bd85686b50b8375a19ca93b0f09ed210355799
7account <DAvidliancongc2789@nycstudents.net>
2025-05-13 13:28:53 +0000

    first commit

---
+++ BetterMath.java
@@ -0,0 +1,61 @@
+public class BetterMath {
+    public static int abs(int num) { 
+        if (num < 0) return -(num);
+        return num;
+    }
+
+    public static double abs(double num) { 
+        if (num < 0.0) return -(num);
+        return num;
+    }
+
+    public static double pow(double base, double exponent) {
+        double count = 0.0;
+        for (int i = 0; i < exponent; i++) {
+            count *= exponent;
+        }
+        return count;
+    }
+
+    public static double sqrt(double x) {
+        double count = x;
+        for (int i = 20; i <= 400; i+= 20) {
+            count = 0.5 * (i + (count / i));
+        }
+        return count;
+    }
+
+    public static double ceil(double x) {
+        int num = ((int) x) + 1;
+        return (double) num;
+    }
+
+    public static double floor(double x) {
+        int num = (int) x;
+        return (double) num;
+    }
+
+    public static double round(double x) {
+        
+    }
+
+    public static int min(int x, int y) {
+        if (x < y) return x;
+        return y;
+    }
+
+    public static int max(int x, int y) {
+        if (x > y) return x;
+        return y;
+    }
+
+    public static double min(double x, double y) {
+        if (x < y) return x;
+        return y;
+    }
+
+    public static double max(double x, double y) {
+        if (x > y) return x;
+        return y;
+    }
+}
